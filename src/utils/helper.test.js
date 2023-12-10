b96942362e88cebcf420b5e9cbae2446d1ed8f33
Reena Rajani <reena@Reenas-MBP.lan>
2023-12-11 09:37:22 +1100

    add the project readme and test setup

---
+++ src/utils/helper.test.js
@@ -0,0 +1,33 @@
+// utils/helper.test.js
+import { filterData } from './helper';
+
+describe('filterData', () => {
+  const mockData = {
+    entries: [
+      { programType: 'series', releaseYear: 2015, title: 'B' },
+      { programType: 'movie', releaseYear: 2012, title: 'A' },
+      { programType: 'series', releaseYear: 2018, title: 'C' },
+    ],
+  };
+
+  it('filters and sorts data correctly for series', () => {
+    const result = filterData({ programType: 'series', data: mockData });
+
+    const expectedResult = [
+      { programType: 'series', releaseYear: 2015, title: 'B' },
+      { programType: 'series', releaseYear: 2018, title: 'C' },
+    ];
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('filters and sorts data correctly for movies', () => {
+    const result = filterData({ programType: 'movie', data: mockData });
+
+    const expectedResult = [
+      { programType: 'movie', releaseYear: 2012, title: 'A' },
+    ];
+
+    expect(result).toEqual(expectedResult);
+  });
+});
